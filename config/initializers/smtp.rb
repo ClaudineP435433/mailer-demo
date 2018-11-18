60ec1e6327d1b779efc543c01e00d896bdfe1b24
Claudine Pradal <claudinecartailler@gmail.com>
2018-11-18 10:45:36 +0100

    change sendgrid config

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'yourdomain.com',
+  :domain => 'mailer-test-batch191.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
